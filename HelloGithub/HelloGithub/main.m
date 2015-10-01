ca98127ccabca8601d796b7e17a55a3e2da883a0
LorenHan <lorenhan@163.com>
2015-10-01 16:57:58 +0800

    第一次提交
    1

---
+++ HelloGithub/HelloGithub/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloGithub
+//
+//  Created by 韩 晓剑 on 15/10/1.
+//  Copyright © 2015年 韩 晓剑. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSLog(@"Hello, Github!");
+    }
+    return 0;
+}
